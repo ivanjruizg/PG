d3e22a1a903ce8e4d3d8e59c291f3e32f3e99777
jr105 <joseriospacheco@gmail.com>
2017-01-10 15:26:06 -0500

    solucion de head

--- application/controllers/coordinador.php
+++ application/controllers/coordinador.php
@@ -10,6 +10,9 @@ class Coordinador extends CI_Controller
         parent::__construct();
         $this->load->model('propuestas_model');
 
+
+
+
         if ($this->session->userdata('tipo') != COORDINADORES) {
 
             redirect(base_url());
